edc8a9b5af2d5cbdc40111418c54cc72a7794e9c
Daniel González <dgonzalez.tagle@gmail.com>
2026-02-01 16:57:19 -0300

    🚀 Factory GTM & Assets Restore: Reparapads (GTM-W9RMTLDS + Missing images)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Orbitron, Rajdhani } from 'next/font/google';
 import { MagneticCursorDot } from '@/components/premium/MagneticCursor';
 import { GrainTexture } from '@/components/premium/GrainTexture';
@@ -126,6 +127,27 @@ export default function Layout({
     return (
 
         <div className={`${orbitron.variable} ${rajdhani.variable} bg-[#050505] text-[#ECFEFF] selection:bg-[#10B981] selection:text-black min-h-screen`}>
+            {/* Google Tag Manager (noscript) */}
+            <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-W9RMTLDS"
+                    height="0"
+                    width="0"
+                    style={{ display: "none", visibility: "hidden" }}
+                />
+            </noscript>
+
+            {/* Google Tag Manager (head script alternative) */}
+            <Script id="gtm" strategy="afterInteractive">
+                {`
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-W9RMTLDS');
+                `}
+            </Script>
+
             {/* SEO & RETENTION SYSTEM */}
             <script
                 type="application/ld+json"
